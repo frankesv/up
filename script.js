79099bcdb08c80e70b69c9b38aa47fd6a7939779
frankesv <89409865+frankesv@users.noreply.github.com>
2025-01-22 16:02:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ document.getElementById("pdf-form").addEventListener("submit", async (event) => 
     // Edytuj dokument
     const pages = pdfDoc.getPages();
     const firstPage = pages[0];
-    firstPage.drawText(`${name}`, { x: 138, y: 207, size: 15 });
-    firstPage.drawText(`${email}`, { x: 90, y: 233, size: 15 });
-    firstPage.drawText(`${phone}`, { x: 165, y: 255, size: 15 });
+    firstPage.drawText(`${name}`, { x: 138, y: 635, size: 15 });
+    firstPage.drawText(`${email}`, { x: 90, y: 609, size: 15 });
+    firstPage.drawText(`${phone}`, { x: 165, y: 587, size: 15 });
 
     // Generuj plik PDF do pobrania
     const pdfBytes = await pdfDoc.save();
